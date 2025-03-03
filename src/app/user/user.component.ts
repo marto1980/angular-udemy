e676cc522915938ac84d8422a4eb9bc5a768bc5a
Martin Ivanov <marto1980@gmail.com>
2025-03-03 14:48:06 +0200

    Add a user component with string interpolation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -6,6 +9,7 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
 
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex]
 }
