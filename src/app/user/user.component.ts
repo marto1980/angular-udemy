b2b32018709b7afe0d2116b83680ca9f2cfdf759
Martin Ivanov <marto1980@gmail.com>
2025-04-13 14:11:21 +0300

    Use output() instead of @Output to emit events

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { Component, Input, output } from '@angular/core'
 
 @Component({
   selector: 'app-user',
@@ -10,7 +10,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string
   @Input({ required: true }) avatar!: string
   @Input({ required: true }) name!: string
-  @Output() select = new EventEmitter<string>()
+
+  select = output<string>()
   get ImagePath() {
     return 'assets/users/' + this.avatar
   }
