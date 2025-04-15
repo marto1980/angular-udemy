2a35fbad4bfe065a9fd1057f1221ec1c50d9f2da
Martin Ivanov <marto1980@gmail.com>
2025-04-15 10:35:06 +0300

    Accept objects as inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,16 +7,18 @@ import { Component, Input, output } from '@angular/core'
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string
-  @Input({ required: true }) avatar!: string
-  @Input({ required: true }) name!: string
+  @Input({ required: true }) user!: {
+    id: string
+    avatar: string
+    name: string
+  }
 
   select = output<string>()
   get ImagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
 
   onSelectUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.user.id)
   }
 }
