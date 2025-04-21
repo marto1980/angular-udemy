3655e33ece134f04416b071c0e343022591cbfd5
Martin Ivanov <marto1980@gmail.com>
2025-04-21 19:19:12 +0300

    Add new resources

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss',
+})
+export class TaskComponent {}
