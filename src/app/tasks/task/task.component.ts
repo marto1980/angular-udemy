e846441a1a794907f10ea91de160120cccebd424
Martin Ivanov <marto1980@gmail.com>
2025-04-25 09:35:41 +0300

    Output Task Data in the Task Component

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
+type Task = {
+  id: string
+  userId: string
+  title: string
+  summary: string
+  dueDate: string
+}
 @Component({
   selector: 'app-task',
   imports: [],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
-export class TaskComponent {}
+export class TaskComponent {
+  @Input({ required: true }) task!: Task
+}
