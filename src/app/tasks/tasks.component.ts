783b34ed93c4485248f11a59ab696e89a6d55844
Martin Ivanov <marto1980@gmail.com>
2025-04-27 18:43:45 +0300

    Add a NewTaskComponent: My Solution

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core'
 
 import { dummyTasks } from './dummy-tasks'
+import { NewTaskComponent } from './new-task/new-task.component'
 import { TaskComponent } from './task/task.component'
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss',
 })
@@ -13,10 +14,14 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string
   @Input({ required: true }) name: string | undefined
   tasks = dummyTasks
+  showNewTask = false
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId)
   }
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+  onAddTask() {
+    this.showNewTask = true
   }
 }
