00ad2b92eccea4c0c64f0e75aa91c568febd995f
Martin Ivanov <marto1980@gmail.com>
2025-04-29 09:32:10 +0300

    Demonstrate Two-Way Binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, Output } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss',
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>()
+  enteredTitle = ''
   onCancel() {
     this.cancel.emit()
   }
